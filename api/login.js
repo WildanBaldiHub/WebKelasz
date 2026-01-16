465e212e1ceaebdafe5fd7ea6c3d1bd8f10d7847
WildanBaldiHub <wildanbaldifans2@gmail.com>
2026-01-16 09:58:49 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,25 +1,14 @@
-function authLogin() {
-    const user = document.getElementById('log-user').value;
-    const pass = document.getElementById('log-pass').value;
-
-    if(user === "admin" && pass === "123") {
-        localStorage.setItem('userRole', 'Admin');
-        localStorage.setItem('userName', 'Super Admin');
-        return masukApp();
-    }
-
-    const found = DATA_SISWA.find(s => s.nama.toLowerCase() === user.toLowerCase() && s.pass === pass);
-    if(found) {
-        localStorage.setItem('userRole', found.role);
-        localStorage.setItem('userName', found.nama);
-        masukApp();
-    } else {
-        alert("Login Gagal! Gunakan Nama Lengkap & pass 'siswa123'");
-    }
-}
-
 function masukApp() {
+    const role = localStorage.getItem('userRole');
+    const name = localStorage.getItem('userName');
+    
     document.getElementById('login-view').classList.add('hide');
     document.getElementById('app-view').classList.remove('hide');
+    
+    // Update Info di Sidebar & Header
+    document.getElementById('user-display-name').innerText = name;
+    document.getElementById('user-role-display').innerText = role;
+    document.getElementById('user-initial').innerText = name.charAt(0);
+    
     showSiswa();
 }
