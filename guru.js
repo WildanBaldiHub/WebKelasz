9d1b2a129ff6d24d443de52ec38626fad0238ff3
WildanBaldiHub <wildanbaldifans2@gmail.com>
2026-01-16 09:57:37 +0700

    Create guru.js

---
+++ guru.js
@@ -0,0 +1,8 @@
+function showGuru() {
+    document.getElementById('main-content').innerHTML = `
+        <h2 class="text-3xl font-black mb-6">Wali Kelas</h2>
+        <div class="bg-indigo-600 text-white p-8 rounded-[2.5rem] shadow-xl">
+            <h3 class="text-2xl font-black">Bpk. Ahmad Sujai</h3>
+            <p class="font-bold opacity-80">NIP. 1928374655</p>
+        </div>`;
+}
